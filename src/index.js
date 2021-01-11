144484d86927fe3d5e3d06a458f4fbb4a2982f2f
jwpf100 <jwpf100@gmail.com>
2021-01-11 18:21:10 +0000

    First commit.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const axios = require('axios');
+
+const testData = [
+  {
+    _id:"5f97d9696c94ef5b0a6a5c47",
+    title:"New Blog Post Title 1",
+    author:
+      {
+        _id:"5f970506ac7954477e5f6fa4",
+        first_name:"Joe",
+        family_name:"Fletcher",
+        date_of_birth:"1983-09-01T00:00:00.000Z",
+        __v:0
+      },
+    summary:"New Blog Post Summary 1"
+  },
+  {
+    _id:"5f97d98b0b18cc5b2db95bef",
+    title:"New Blog Post Title 2",
+    "author":
+      {
+        "_id":"5f970506ac7954477e5f6fa4",
+        first_name:"Joe",
+        family_name:"Fletcher",
+        date_of_birth:"1983-09-01T00:00:00.000Z",
+        __v:0
+      },
+    summary:"New Blog Post Summary 2"
+  }
+]
+
+
+async function getBlogs() {
+  try {
+    const response = await axios.get(`http://localhost:4000/api/blogposts`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getBlogs()
+
+
+const BlogList = props => (
+  <div>
+    {props.blogArray.map(blog => <Blog blogInfo={blog} />)}
+  </div>
+)
+
+
+const Blog = props => {
+  const blogData = props.blogInfo
+  return (
+    <div>
+      <div>{blogData.title}</div>
+      <div>{blogData.summary}</div>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <BlogList blogArray={testData}/>
+  </div>,
   document.getElementById('root')
 );
 
