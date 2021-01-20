047d0959fb4c32a0f2861c964bedf2bc327b7ae6
jwpf100 <jwpf100@gmail.com>
2021-01-20 10:02:51 +0000

    Changed date formatting. Added Luxon.

--- src/Components/BlogHeader.js
+++ src/Components/BlogHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { DateTime } from "luxon";
 
 const BlogHeader = props => {
   const blogData = props.blogArray
@@ -12,6 +13,7 @@ const BlogHeader = props => {
         <div className="order-md-0 col-md-6 px-0">
           <h1 className="display-4 font-italic text-dark">{blogData.title}</h1>
           <p className="lead text-dark my-3">{blogData.summary}</p>
+          <p className="text-dark my-3">{DateTime.fromISO(blogData.post_date).toLocaleString(DateTime.DATE_MED)}</p>
           <Link to={{
             pathname:`/${blogData._id}`, 
             transferData: {
--- src/Components/FeaturedPost.js
+++ src/Components/FeaturedPost.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { DateTime } from "luxon";
 
 const FeaturedPost = props => {
   const blogData = props.blogInfo
   const tagList = blogData.tags.map(tag => tag.name)
-  
+
   return (
   
   <div className="col-md-6">
@@ -13,7 +14,7 @@ const FeaturedPost = props => {
           <h3 className="mb-0">{blogData.title}</h3>
           <img className="rounded-circle bg-white align-self-center" src={`${props.image_url}${blogData.image_filename}`} alt="Generic placeholder" width="150" height="150" /> 
           <strong className="d-inline-block mb-2 text-muted">{tagList.join(' / ')}</strong>
-          <div className="mb-1 text-muted">{blogData.post_date}</div>
+          <div className="mb-1 text-muted">{DateTime.fromISO(blogData.post_date).toLocaleString(DateTime.DATE_MED)}</div>
           <p className="mb-auto">{blogData.summary}</p>
           <Link to={{
             pathname:`/${blogData._id}`, 
